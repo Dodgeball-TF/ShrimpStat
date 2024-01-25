ec461094dc6bac5138988feb2f3112ed8a236c15
QtKai <15091327+QtKaii@users.noreply.github.com>
2024-01-25 09:15:25 +0000

    Begin UnclassifiedEvent::from_log_line and UnclassifiedEvent::from_extension

--- api/src/event_handler.rs
+++ api/src/event_handler.rs
@@ -19,3 +19,15 @@ pub enum UnclassifiedEvent{
   ServerRegister
 }
 
+impl UnclassifiedEvent{
+  pub fn from_log_line(raw: &str) -> Self {
+    todo!("Implement UnclassifiedEvent::from_log_line");
+  }
+
+  pub async fn from_extension(raw: &str) -> Self {
+    // dummy sleep to make the compiler happy
+    tokio::time::sleep(tokio::time::Duration::from_secs(0)).await;
+    todo!("Implement UnclassifiedEvent::from_extension");
+  }
+}
+
